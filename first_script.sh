01c25b7e8010468533659c83ca61be6656182fb3
Ubuntu <ubuntu@ip-172-31-87-7.ec2.internal>
2025-07-02 13:43:18 +0000

    added first script

---
+++ first_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+	
+echo "chatur: Adarniya sabhapati mahodaya"
+
+echo "chatur: aaj hum sab mil ke chamatkaar karenge"
+
+echo "rancho: ratta marega to aisa hi hoga"
+
+echo "chatur: thuchuk thuchuk"
+
+echo "raju: chal ek folder bana ke dikha"
+
+mkdir -p rancho
+
+echo "rancho: ye leh, bana diya"
+
+echo "farhan: virus ne beizati kar di, bola farhannitrate"
+
+echo "abba nahi manenge" > farhannitrate.txt
